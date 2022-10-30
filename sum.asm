d97bbfc01567c6acec1c7b6ed666aafd342595c7
Ludmila Silveira <105131002+Ludmilahttps@users.noreply.github.com>
2022-10-30 18:57:51 -0300

    Soma considerando apenas o carry de dentro

--- sum.asm
+++ sum.asm
@@ -1,19 +1,26 @@
 .org 0024h
 
-	;soma os mais significativos e add em 205CH
-	LDA 2051h      ;carrega o número da memória para o acumulador.
-	LXI H, 2053h   ;carrega os registradores.
-	ADD M          ;soma do ac para B.
-	STA 205Ch      ;Armazena o conteúdo do ac em 205Ch.
-	JC L2          ;Pula para L2 se tiver carry
-
-	;soma os menos significativos e add em 205BH
+	;soma os menos significativos e add em 205CH
 	LDA 2050h      ;carrega o número da memória para o acumulador.
 	LXI H, 2052h   ;carrega os registradores.
 	ADD M          ;soma do ac para B.
-	STA 205Bh      ;Armazena o conteúdo do ac em 205Bh.
+	STA 205Bh      ;Armazena o conteúdo do ac em 205Ch.
+	MVI D,00H      ;Zera o registrador D
+	JC L2          ;Pula para L2 se tiver carry
 
-L2:   
+L1:
+	;soma os mais significativos e add em 205BH
+	LDA 2051h      ;carrega o número da memória para o acumulador
+	ADD D
+	LXI H, 2053h   ;carrega os registradores.
+	ADD M          ;soma do ac para B.
+	STA 205Ch      ;Armazena o conteúdo do ac em 205Bh.
+	JMP L3         ;pule para L3
+
+L2:   INR D          ;incrementa 1 em D
+      JMP L1         ;pule para L1
+
+L3:
 	LXI H, 2053h   ;carrega os registradores;
 	INR D          ;incrementador
 	MOV M, D       ;transferência do conteúdo.
